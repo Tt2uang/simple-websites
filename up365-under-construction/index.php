6b3f232e89830e7f1a691b9a22b8682f92b82220
Trần Thế Quang <tt2uang@gmail.com>
2019-03-02 19:36:27 +0700

    modify index.php

--- up365-under-construction/index.php
+++ up365-under-construction/index.php
@@ -1,21 +1,26 @@
 <!DOCTYPE html>
 <?php
-$url = $_SERVER['HTTP_HOST'].$_SERVER['HTTP_SELF'];
-if (strpos($url, 'solution365') !== FALSE)
-	$site = 'solution365';
+$url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+if (strpos($url, 'sol365') !== FALSE)
+	$site = 'sol365';
+elseif (strpos($url, 'solution365') !== FALSE)
+	$site = 'sol365';
 elseif (strpos($url, 'light365') !== FALSE)
 	$site = 'light365';
 elseif (strpos($url, 'sang365') !== FALSE)
 	$site = 'light365';
 elseif (strpos($url, 'crystal365') !== FALSE)
 	$site = 'crystal365';
-elseif (strpos($url, 'solution365') !== FALSE)
-	$site = 'solution365';
+elseif (strpos($url, 'edu365') !== FALSE)
+	$site = 'edu365';
+elseif (strpos($url, 'shop365') !== FALSE)
+	$site = 'shop365';
 else
 	$site = 'up365';
 ?>
 
-<html style="">
+<html>
 <head>
 	<link rel="icon" href="images/favicon-<?=$site?>.ico" type="image/x-icon">
 	<style>
